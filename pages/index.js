0e73dccdce81a7ee28c5b8ad90843caf49da4bbf
seckmohamedelbachir7-sketch <seckmohamedelbachir7@gmail.com>
2026-03-11 16:21:15 +0100

    Create pages/index.js for the PharmacistApp React component

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const PharmacistApp = () => {
+    return (
+        <div>
+            <h1>Welcome to the Pharmacist App</h1>
+            <p>This is the homepage for all your pharmacy needs.</p>
+        </div>
+    );
+};
+
+export default PharmacistApp;
