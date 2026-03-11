91ddaf460b10ae84bce7c50009e658057c2031f2
seckmohamedelbachir7-sketch <seckmohamedelbachir7@gmail.com>
2026-03-11 16:19:54 +0100

    Add next.config.js for Next.js configuration

---
+++ next.config.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  // Add your custom configuration here
+};
+
+module.exports = nextConfig;
